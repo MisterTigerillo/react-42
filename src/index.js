e984f59c66266a3e49c0c5937440a362fda35d6b
Dmytro Gobosh <88391310+MisterTigerillo@users.noreply.github.com>
2022-08-21 13:32:25 +0300

    misc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const croot = document.getElementById('root');
+
+const Prime = () => {
+  const third = 'stark';
+  return (
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+      <li>{third}</li>
+    </ul>
+  );
+};
+
+ReactDOM.render(<Prime />, croot);
