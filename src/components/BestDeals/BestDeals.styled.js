526a9c6b4a6abc72ae2a53232deb90114d70ca62
Dmytro Gobosh <88391310+MisterTigerillo@users.noreply.github.com>
2024-02-12 19:18:28 +0200

    +misc

--- src/components/BestDeals/BestDeals.styled.js
+++ src/components/BestDeals/BestDeals.styled.js
@@ -2,18 +2,37 @@ import styled from 'styled-components';
 
 export const Styled = {
   GalleryButtons: styled.div`
+    outline: 1px solid black;
+    position: relative;
     display: flex;
     justify-content: center;
     background-color: lightcoral;
-    margin-bottom: 25px;
+    margin-bottom: 50px;
   `,
   Button: styled.button`
     width: 25%;
     height: 50px;
   `,
 
+  Tab: styled.div`
+    background-color: lightgray;
+    position: absolute;
+    width: 25%;
+    top: 102%;
+    text-align: center;
+    height: 3rem;
+    line-height: 3rem;
+    /* left: 0; */
+  `,
+
   GalleryTabs: styled.div`
     /* margin-top: 20px; */
     background-color: lightblue;
+  `,
+  Tabs: styled.div`
+    display: flex;
+    justify-content: center;
+    column-gap: 30px;
+    background-color: lightseagreen;
   `,
 };
--- src/components/BestDeals/index.jsx
+++ src/components/BestDeals/index.jsx
@@ -3,17 +3,44 @@ import React, { Component } from 'react';
 import { Wrapper } from './Wrapper.styled';
 import { Styled } from './BestDeals.styled';
 
+const GalleryOptions = [
+  { label: 'Pic1', tagOne: 'Featured', tagTwo: '3D' },
+  { label: 'Pic2', tagOne: 'Featured' },
+  { label: 'Pic3', tagTwo: '3D' },
+];
+
 class BestDeals extends Component {
-  state: {visible: false,}
+  state = { visible: false };
+
+  toggle = () => {
+    this.setState(prevState => ({ visible: !prevState.visible }));
+  };
 
   render() {
-    const { GalleryButtons, GalleryTabs, Button } = Styled;
+    const { GalleryButtons, Button, Tab, Tabs } = Styled;
     return (
       <Wrapper>
         <GalleryButtons>
-          <Button>Show</Button>
+          <Button onClick={this.toggle}>
+            {this.state.visible ? 'Hide' : 'Show'}
+          </Button>
+          {this.state.visible && (
+            <Tab>
+              <div>
+                <a href="">
+                  <img src="../../assets/Image.jpg" alt="House" />
+                </a>
+              </div>
+            </Tab>
+          )}
         </GalleryButtons>
-        <GalleryTabs></GalleryTabs>
+        <Tabs>
+          <button onClick={this}>Residential Property</button>
+          <button>Commercial Property</button>
+          <button>Agriculture Property</button>
+          <button>Industrial Property</button>
+        </Tabs>
+        {/* <GalleryTabs></GalleryTabs> */}
       </Wrapper>
     );
   }
