037ca6ab1c1e09c77c66dc5f1b721909a22b238d
Dmytro Gobosh <irulka103@ukr.net>
2024-02-17 23:45:02 +0200

    +misc

--- src/components/BestDeals/index.jsx
+++ src/components/BestDeals/index.jsx
@@ -3,10 +3,30 @@ import React, { Component } from 'react';
 import { Wrapper } from './Wrapper.styled';
 import { Styled } from './BestDeals.styled';
 
-const GalleryOptions = [
-  { label: 'Pic1', tagOne: 'Featured', tagTwo: '3D' },
-  { label: 'Pic2', tagOne: 'Featured' },
-  { label: 'Pic3', tagTwo: '3D' },
+const tabsCarousel = [
+  [
+    { id: 'H1', label: 'Pic1', tags: ['Featured', '3D'] },
+    { id: 'H2', label: 'Pic2', tags: ['Featured', '3D'] },
+    { id: 'H3', label: 'Pic3', tags: '3D' },
+  ],
+
+  [
+    { id: 'H4', label: 'Pic1', tags: ['Featured', '3D'] },
+    { id: 'H5', label: 'Pic2', tags: '3D' },
+    { id: 'H6', label: 'Pic3', tags: ['Featured', '3D'] },
+    { id: 'H7', label: 'Pic3', tags: '3D' },
+    { id: 'H8', label: 'Pic3', tags: ['Featured', '3D'] },
+    { id: 'H9', label: 'Pic3', tags: '3D' },
+  ],
+
+  [
+    { id: 'H10', label: 'Pic1', tags: ['Featured', '3D'] },
+    { id: 'H11', label: 'Pic2', tags: '3D' },
+  ],
+  [
+    { id: 'H12', label: 'Pic1', tags: ['Featured', '3D'] },
+    { id: 'H13', label: 'Pic2', tagOne: ['Featured', '3D'] },
+  ],
 ];
 
 class BestDeals extends Component {
@@ -24,23 +44,15 @@ class BestDeals extends Component {
           <Button onClick={this.toggle}>
             {this.state.visible ? 'Hide' : 'Show'}
           </Button>
-          {this.state.visible && (
-            <Tab>
-              <div>
-                <a href="">
-                  <img src="../../assets/Image.jpg" alt="House" />
-                </a>
-              </div>
-            </Tab>
-          )}
         </GalleryButtons>
         <Tabs>
-          <button onClick={this}>Residential Property</button>
+          <button>Residential Property</button>
           <button>Commercial Property</button>
           <button>Agriculture Property</button>
           <button>Industrial Property</button>
         </Tabs>
         {/* <GalleryTabs></GalleryTabs> */}
+        {tabsCarousel.map(tab => console.log(tab))}
       </Wrapper>
     );
   }
